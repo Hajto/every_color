fe42059d6fb13a181dd30f111f982ff966c34418
Haito <hajtosek@gmail.com>
2016-11-01 16:28:57 +0100

    Fixed socket

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -2,6 +2,6 @@ defmodule EveryColor.PageController do
   use EveryColor.Web, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    html(conn, File.read!("priv/static/index.html"))
   end
 end
