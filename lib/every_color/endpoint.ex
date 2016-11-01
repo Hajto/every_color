de4930dd762443d9162752f53de142f1e1d4c739
Tomasz Cichocinski <tomaszcichocinski@gmail.com>
2016-11-01 16:23:29 +0100

    connect to frontend

--- lib/every_color/endpoint.ex
+++ lib/every_color/endpoint.ex
@@ -9,7 +9,7 @@ defmodule EveryColor.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :every_color, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    only: ~w(css fonts images js favicon.ico robots.txt index.html static)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
